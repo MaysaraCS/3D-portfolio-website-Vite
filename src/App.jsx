59c722bd7b4abd17329a391f1a266c60c148d91d
Maysara <maysaracs1001@gmail.com>
2025-09-04 17:21:39 +0300

    NavBar-hero txt

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-// import Navbar from "./sections/navbar";
-// import Hero from "./sections/Hero";
-// import About from "./sections/About";
-// import Projects from "./sections/Projects";
-// import Experiences from "./sections/Experiences";
-// import Testimonial from "./sections/Testimonial";
-// import Contact from "./sections/Contact";
-// import Footer from './sections/Footer';
+import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
+import About from "./sections/About";
+import Projects from "./sections/Projects";
+import Experiences from "./sections/Experiences";
+import Testimonial from "./sections/Testimonial";
+import Contact from "./sections/Contact";
+import Footer from './sections/Footer';
 
 const App = () => {
   return (
     <div className="container mx-auto max-w-7xl">Hello
-      {/* <Navbar />
+      <Navbar />
       <Hero />
       <About />
       <Projects />
       <Experiences />
       <Testimonial />
       <Contact />
-      <Footer/> */}
+      <Footer/>
     </div>
   );
 };
